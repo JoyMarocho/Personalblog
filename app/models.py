792adc854033a204e29b7da7f8aca426d84dd1b8
JoyMarocho <joymarocho@gmail.com>
2022-05-16 11:23:17 +0300

    creating files

--- app/models.py
+++ app/models.py
@@ -157,4 +157,4 @@ class Quote:
     """
     def __init__(self, author, quote):
         self.author = author
-        self.quote = quote
+        self.quote = quote                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
